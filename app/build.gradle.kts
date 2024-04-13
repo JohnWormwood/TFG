77ccd9f8f887cb11a0bd3fb82c82fdba96d37b7a
AngelCen14 <angelcen1447@gmail.com>
2024-04-13 16:01:09 +0200

    - Añadida la dependencia de lombok - Cambiado el sdk de compilacion del 33 al 34 porque si no no se ejecuta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.tfg"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.tfg"
@@ -39,4 +39,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Lombok
+    compileOnly ("org.projectlombok:lombok:1.18.32")
+    annotationProcessor ("org.projectlombok:lombok:1.18.32")
 }
