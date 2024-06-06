606d903148aca6f7e2593a6957e3db4a6776daa3
AngelCen14 <angelcen1447@gmail.com>
2024-06-06 20:29:27 +0200

    Arreglo bug - La granja no tenia generador de comida, ahora ya lo tiene

--- app/src/main/java/com/tfg/modelos/edificios/Granja.java
+++ app/src/main/java/com/tfg/modelos/edificios/Granja.java
@@ -1,12 +1,14 @@
 package com.tfg.modelos.edificios;
 
 import com.tfg.modelos.Aldea;
+import com.tfg.modelos.enums.RecursosEnum;
+import com.tfg.modelos.generadores_recursos.impl.GeneradorEstandar;
 
 public class Granja extends Edificio {
     public Granja(int aldeanosAsignados, Aldea aldea) {
         super(aldeanosAsignados, aldea);
         desbloqueado = false;
         generarRecursosConstantemente = true;
+        generadoresRecursos.add(new GeneradorEstandar(RecursosEnum.COMIDA));
     }
-
 }
